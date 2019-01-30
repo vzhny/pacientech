41d948e69cb50c1f941fa9b04029d14ecfacbc3d
Diego Vizhnay <diego@vzhny.com>
2019-01-29 23:20:35 -0500

    Implemented useState hook for the navbar toggle | 01/29

--- src/components/Navigation/Navigation.js
+++ src/components/Navigation/Navigation.js
@@ -1,10 +1,18 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { Button } from 'reactstrap';
 
-const Navigation = () => (
-  <div>
-    <p>Navigation Component</p>
-  </div>
-);
+const Navigation = () => {
+  const [isOpen, toggleNavbar] = useState(false);
+
+  return (
+    <div style={{ width: '180px' }}>
+      {isOpen ? <p>isOpen is currently true.</p> : <p>isOpen is currently false.</p>}
+      <Button color="primary" block onClick={() => toggleNavbar(!isOpen)}>
+        Toggle
+      </Button>
+    </div>
+  );
+};
 
 Navigation.propTypes = {};
 
--- src/containers/App/App.js
+++ src/containers/App/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import { Router } from '@reach/router';
+import Navigation from '@/components/Navigation/Navigation';
 import Landing from '@/pages/Landing/Landing';
 import NotFound from '@/pages/NotFound/NotFound';
 import styles from './App.module.scss';
 
 const App = () => (
   <div className={styles.App}>
+    <Navigation />
     <Router>
       <Landing path="/" />
       <NotFound default />
