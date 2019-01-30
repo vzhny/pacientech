f30acf7e5b00dd222d555501175e1305a38e5503
Diego Vizhnay <diego@vzhny.com>
2019-01-30 13:57:24 -0500

    Implemented a user hook in the App container and created the UserContext file | 01/30

--- src/containers/App/App.js
+++ src/containers/App/App.js
@@ -1,18 +1,23 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Router } from '@reach/router';
+import UserContext from '@/auth/UserContext';
 import Navigation from '@/components/Navigation/Navigation';
 import Landing from '@/pages/Landing/Landing';
 import NotFound from '@/pages/NotFound/NotFound';
 import styles from './App.module.scss';
 
-const App = () => (
-  <>
-    <Navigation />
-    <Router className={styles.Main}>
-      <Landing path="/" />
-      <NotFound default />
-    </Router>
-  </>
-);
+const App = () => {
+  const [user, setUser] = useState(null); // eslint-disable-line
+
+  return (
+    <UserContext.Provider value={user}>
+      <Navigation />
+      <Router className={styles.Main}>
+        <Landing path="/" />
+        <NotFound default />
+      </Router>
+    </UserContext.Provider>
+  );
+};
 
 export default App;
