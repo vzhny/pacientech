67c5c2cbcfd7fc64dfca4fbbd1389fbcb5fbde88
Diego Vizhnay <diego@vzhny.com>
2019-01-31 00:30:29 -0500

    Added dashboard page, beginning route guard logic | 01/31

--- src/containers/App/App.js
+++ src/containers/App/App.js
@@ -3,11 +3,12 @@ import { Router } from '@reach/router';
 import UserContext from '@/auth/UserContext';
 import Navigation from '@/components/Navigation/Navigation';
 import Landing from '@/pages/Landing/Landing';
+import Dashboard from '@/pages/Dashboard/Dashboard';
 import NotFound from '@/pages/NotFound/NotFound';
 import styles from './App.module.scss';
 
 const App = () => {
-  const [user, setUser] = useState(null); // eslint-disable-line
+  const [user, setUser] = useState(false); // eslint-disable-line
 
   return (
     <UserContext.Provider value={user}>
---
+++ src/pages/Dashboard/Dashboard.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const Dashboard = () => (
+  <div>
+    <p>Dashboard Component</p>
+  </div>
+);
+
+export default Dashboard;
