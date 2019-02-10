b132d0332aa269aba24e3dbcb23833c92b7e1315
Diego Vizhnay <diego@vzhny.com>
2019-02-10 15:21:32 -0500

    Added a base url pointing to the pacientech api | 02/10

--- src/index.js
+++ src/index.js
@@ -1,7 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import axios from 'axios';
 import '@/styles/scss/base.scss';
 import 'bootswatch/dist/lux/bootstrap.min.css';
 import App from '@/containers/App/App';
+
+axios.defaults.baseURL = 'https://pacientech.herokuapp.com/api';
 
 ReactDOM.render(<App />, document.getElementById('app-root'));
