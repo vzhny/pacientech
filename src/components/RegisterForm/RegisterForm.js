95bf575e543ee36d65450d78ee3df447f2a7f873
Diego Vizhnay <diego@vzhny.com>
2019-02-10 15:24:47 -0500

    Added axios and register logic, updated loader display logic | 02/10

--- src/components/RegisterForm/RegisterForm.js
+++ src/components/RegisterForm/RegisterForm.js
@@ -1,5 +1,7 @@
 import React, { useContext } from 'react';
 import { navigate } from '@reach/router';
+import axios from 'axios';
+import store from 'store';
 import { Row, Col, Form, FormGroup, FormFeedback, Label, Input, Button } from 'reactstrap';
 import { Formik } from 'formik';
 import PropTypes from 'prop-types';
@@ -24,124 +26,129 @@ const RegisterSchema = Yup.object().shape({
 });
 
 const RegisterForm = ({ toggleModal }) => {
-  const [authStatus, updateAuthStatus] = useContext(AuthContext); // eslint-disable-line
+  const [auth, setAuth] = useContext(AuthContext); // eslint-disable-line
 
   return (
     <Formik
       initialValues={{ firstName: '', lastName: '', email: '', password: '' }}
       validationSchema={RegisterSchema}
       onSubmit={(values, { setSubmitting, setErrors }) => {
-        setTimeout(() => {
-          // TODO: add axios call to the pacientech api to log in the user.
-          toggleModal();
-          updateAuthStatus(true);
-          navigate('/dashboard');
-          setSubmitting(false);
-        }, 2000);
+        axios
+          .post('/auth/register', values)
+          .then(res => {
+            const { data } = res;
+            const { firstName, lastName, auth: status, token } = data;
+
+            store.set('user', { firstName, lastName });
+            store.set('token', token);
+
+            setAuth({ status, token });
+            navigate('/dashboard');
+            setSubmitting(false);
+            toggleModal();
+          })
+          .catch(error => {
+            setSubmitting(false);
+          });
       }}
-      render={({
-        values,
-        errors,
-        touched,
-        handleSubmit,
-        handleChange,
-        handleBlur,
-        isSubmitting,
-      }) => (
-        <Form onSubmit={e => handleSubmit(e)}>
-          <Row form>
-            <Col md={6}>
-              <FormGroup>
-                <Label className="text-primary" for="email">
-                  First Name
-                </Label>
-                <Input
-                  className={styles.InputBorder}
-                  type="text"
-                  name="firstName"
-                  onChange={handleChange}
-                  onBlur={handleBlur}
-                  value={values.firstName}
-                  placeholder="John"
-                  disabled={isSubmitting}
-                  valid={!errors.firstName && touched.firstName}
-                  invalid={!!errors.firstName}
-                />
-                {errors.firstName && touched.firstName ? (
-                  <FormFeedback>{errors.firstName}</FormFeedback>
-                ) : null}
-              </FormGroup>
-            </Col>
-            <Col md={6}>
-              <FormGroup>
-                <Label className="text-primary" for="email">
-                  Last Name
-                </Label>
-                <Input
-                  className={styles.InputBorder}
-                  type="text"
-                  name="lastName"
-                  onChange={handleChange}
-                  onBlur={handleBlur}
-                  value={values.lastName}
-                  placeholder="Doe"
-                  disabled={isSubmitting}
-                  valid={!errors.lastName && touched.lastName}
-                  invalid={!!errors.lastName}
-                />
-                {errors.lastName && touched.lastName ? (
-                  <FormFeedback>{errors.lastName}</FormFeedback>
-                ) : null}
-              </FormGroup>
-            </Col>
-          </Row>
-          <FormGroup>
-            <Label className="text-primary" for="email">
-              Email Address
-            </Label>
-            <Input
-              className={styles.InputBorder}
-              type="text"
-              name="email"
-              onChange={handleChange}
-              onBlur={handleBlur}
-              value={values.email}
-              placeholder="johndoe@gmail.com"
-              disabled={isSubmitting}
-              valid={!errors.email && touched.email}
-              invalid={!!errors.email}
-            />
-            {errors.email && touched.email ? <FormFeedback>{errors.email}</FormFeedback> : null}
-          </FormGroup>
-          <FormGroup>
-            <Label className="text-primary" for="password">
-              Password
-            </Label>
-            <Input
-              className={styles.InputBorder}
-              type="password"
-              name="password"
-              onChange={handleChange}
-              onBlur={handleBlur}
-              value={values.password}
-              placeholder="●●●●●●●●"
-              disabled={isSubmitting}
-              valid={!errors.password && touched.password}
-              invalid={!!errors.password}
-            />
-            {errors.password && touched.password ? (
-              <FormFeedback>{errors.password}</FormFeedback>
-            ) : null}
-          </FormGroup>
-          {isSubmitting ? <Loader /> : null}
-          <Button type="submit" color="primary" className="mr-3" disabled={isSubmitting}>
-            Log In
-          </Button>
-          <Button color="secondary" onClick={() => toggleModal()} disabled={isSubmitting}>
-            Cancel
-          </Button>
-        </Form>
-      )}
+      render={({ values, errors, touched, handleSubmit, handleChange, handleBlur, isSubmitting }) =>
+        isSubmitting ? (
+          <Loader />
+        ) : (
+          <Form onSubmit={e => handleSubmit(e)}>
+            <Row form>
+              <Col md={6}>
+                <FormGroup>
+                  <Label className="text-primary" for="email">
+                    First Name
+                  </Label>
+                  <Input
+                    className={styles.InputBorder}
+                    type="text"
+                    name="firstName"
+                    onChange={handleChange}
+                    onBlur={handleBlur}
+                    value={values.firstName}
+                    placeholder="John"
+                    disabled={isSubmitting}
+                    valid={!errors.firstName && touched.firstName}
+                    invalid={!!errors.firstName}
+                  />
+                  {errors.firstName && touched.firstName ? (
+                    <FormFeedback>{errors.firstName}</FormFeedback>
+                  ) : null}
+                </FormGroup>
+              </Col>
+              <Col md={6}>
+                <FormGroup>
+                  <Label className="text-primary" for="email">
+                    Last Name
+                  </Label>
+                  <Input
+                    className={styles.InputBorder}
+                    type="text"
+                    name="lastName"
+                    onChange={handleChange}
+                    onBlur={handleBlur}
+                    value={values.lastName}
+                    placeholder="Doe"
+                    disabled={isSubmitting}
+                    valid={!errors.lastName && touched.lastName}
+                    invalid={!!errors.lastName}
+                  />
+                  {errors.lastName && touched.lastName ? (
+                    <FormFeedback>{errors.lastName}</FormFeedback>
+                  ) : null}
+                </FormGroup>
+              </Col>
+            </Row>
+            <FormGroup>
+              <Label className="text-primary" for="email">
+                Email Address
+              </Label>
+              <Input
+                className={styles.InputBorder}
+                type="text"
+                name="email"
+                onChange={handleChange}
+                onBlur={handleBlur}
+                value={values.email}
+                placeholder="johndoe@gmail.com"
+                disabled={isSubmitting}
+                valid={!errors.email && touched.email}
+                invalid={!!errors.email}
+              />
+              {errors.email && touched.email ? <FormFeedback>{errors.email}</FormFeedback> : null}
+            </FormGroup>
+            <FormGroup>
+              <Label className="text-primary" for="password">
+                Password
+              </Label>
+              <Input
+                className={styles.InputBorder}
+                type="password"
+                name="password"
+                onChange={handleChange}
+                onBlur={handleBlur}
+                value={values.password}
+                placeholder="●●●●●●●●"
+                disabled={isSubmitting}
+                valid={!errors.password && touched.password}
+                invalid={!!errors.password}
+              />
+              {errors.password && touched.password ? (
+                <FormFeedback>{errors.password}</FormFeedback>
+              ) : null}
+            </FormGroup>
+            <Button type="submit" color="primary" className="mr-3" disabled={isSubmitting}>
+              Register
+            </Button>
+            <Button color="secondary" onClick={() => toggleModal()} disabled={isSubmitting}>
+              Cancel
+            </Button>
+          </Form>
+        )
+      }
     />
   );
 };
