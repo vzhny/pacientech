bdb83c3018eaeca3f85cba01ee41f13eeeeaa107
Diego Vizhnay <diego@vzhny.com>
2019-02-10 15:26:01 -0500

    Updated class names logic, added a dashboard button available on login, added logout logic | 02/10

--- src/components/Navigation/Navigation.js
+++ src/components/Navigation/Navigation.js
@@ -1,4 +1,8 @@
 import React, { useState, useContext } from 'react';
+import { Link, navigate } from '@reach/router';
+import axios from 'axios';
+import store from 'store';
+import clsx from 'clsx';
 import {
   Container,
   Navbar,
@@ -18,7 +22,7 @@ import styles from './Navigation.module.scss';
 const Navigation = () => {
   const [isOpen, toggleNavbar] = useState(false);
   const [modalVisible, toggleModal] = useState(false);
-  const [authStatus, updateAuthStatus] = useContext(AuthContext);
+  const [auth, setAuth] = useContext(AuthContext);
   const [modal, setModalProps] = useState({});
 
   const openLoginFormModal = () => {
@@ -38,35 +42,63 @@ const Navigation = () => {
   };
 
   const logOutUser = () => {
-    setTimeout(() => {
-      updateAuthStatus(false);
-    }, 1000);
+    axios
+      .post('/auth/logout')
+      .then(res => {
+        store.remove('token');
+        store.remove('user');
+        setAuth({ status: false, token: '' });
+        navigate('/');
+      })
+      .catch(error => {});
   };
 
   return (
     <>
       <Navbar color="dark" dark expand="md">
         <Container>
-          <NavbarBrand href="/">pacientech</NavbarBrand>
+          <NavbarBrand
+            tag={() => (
+              <Link className="navbar-brand" to="/">
+                pacientech
+              </Link>
+            )}
+          />
           <NavbarToggler onClick={() => toggleNavbar(!isOpen)} />
           <Collapse isOpen={isOpen} navbar>
             <Nav className="ml-auto" navbar>
-              {authStatus ? (
-                <NavItem>
-                  <NavLink className={styles.LogOutButton} onClick={() => logOutUser()}>
-                    Log Out
-                  </NavLink>
-                </NavItem>
+              {auth.status ? (
+                <>
+                  <NavItem>
+                    <NavLink
+                      className={clsx('text-light', styles.CursorOnHover)}
+                      onClick={() => navigate('/dashboard')}
+                    >
+                      Dashboard
+                    </NavLink>
+                  </NavItem>
+                  <NavItem>
+                    <NavLink
+                      className={clsx('text-light', styles.CursorOnHover)}
+                      onClick={() => logOutUser()}
+                    >
+                      Log Out
+                    </NavLink>
+                  </NavItem>
+                </>
               ) : (
                 <>
                   <NavItem>
-                    <NavLink className={styles.LogInButton} onClick={() => openLoginFormModal()}>
+                    <NavLink
+                      className={clsx('text-light', styles.CursorOnHover)}
+                      onClick={() => openLoginFormModal()}
+                    >
                       Log In
                     </NavLink>
                   </NavItem>
                   <NavItem>
                     <NavLink
-                      className={styles.RegisterButton}
+                      className={clsx('text-light', styles.CursorOnHover)}
                       onClick={() => openRegisterFormModal()}
                     >
                       Register
