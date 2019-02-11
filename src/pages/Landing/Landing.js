7998ee9be7ababc3720729c86497da8d243ede14
Diego Vizhnay <diego@vzhny.com>
2019-02-11 18:31:50 -0500

    Added Landing page, currently has lorem placeholder text | 02/11

--- src/pages/Landing/Landing.js
+++ src/pages/Landing/Landing.js
@@ -1,5 +1,49 @@
 import React from 'react';
+import clsx from 'clsx';
+import { Jumbotron, Container, Row, Col } from 'reactstrap';
+import DoctorWithPatient from '@/images/doctor-with-patient.svg';
+import Clipboards from '@/images/clipboards.svg';
+import DataDisplay from '@/images/data-display.svg';
+import styles from './Landing.module.scss';
 
-const Landing = () => <h1>Landing Page</h1>;
+const Landing = () => (
+  <Jumbotron className={styles.FullHeight}>
+    <Container>
+      <Row>
+        <Col className="d-flex">
+          <img className={styles.Image} src={DoctorWithPatient} alt="Doctor with Patient" />
+        </Col>
+        <Col className="d-flex">
+          <p className={clsx('text-primary', styles.Blurb)}>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Autem, aspernatur minima modi
+            quis repellat ullam quaerat nesciunt perspiciatis quos tenetur.
+          </p>
+        </Col>
+      </Row>
+      <Row>
+        <Col className="d-flex">
+          <p className={clsx('text-primary', styles.Blurb)}>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Inventore quam quasi quos
+            quibusdam ullam, nam harum voluptatum necessitatibus at deserunt.
+          </p>
+        </Col>
+        <Col className="d-flex">
+          <img className={styles.Image} src={Clipboards} alt="Clipboards" />
+        </Col>
+      </Row>
+      <Row>
+        <Col className="d-flex">
+          <img className={styles.Image} src={DataDisplay} alt="Data Display" />
+        </Col>
+        <Col className="d-flex">
+          <p className={clsx('text-primary', styles.Blurb)}>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Possimus, odit deleniti tenetur
+            in quidem unde voluptas porro ipsam quibusdam perferendis.
+          </p>
+        </Col>
+      </Row>
+    </Container>
+  </Jumbotron>
+);
 
 export default Landing;
