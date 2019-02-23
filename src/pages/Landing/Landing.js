5b0572d2abe9727eb069121bb1ab5e316b68f00e
Diego Vizhnay <diego@vzhny.com>
2019-02-22 20:11:27 -0500

    Added window width check in order to position middle image and blurb properly on mobile | 02/22

--- src/pages/Landing/Landing.js
+++ src/pages/Landing/Landing.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import clsx from 'clsx';
 import { Jumbotron, Container, Row, Col } from 'reactstrap';
 import DoctorWithPatient from '@/images/doctor-with-patient.svg';
@@ -6,44 +6,66 @@ import Clipboards from '@/images/clipboards.svg';
 import DataDisplay from '@/images/data-display.svg';
 import { fullHeight, image, blurb } from './Landing.module.scss';
 
-const Landing = () => (
-  <Jumbotron className={fullHeight}>
-    <Container>
-      <Row>
-        <Col className="d-flex">
-          <img alt="Doctor with Patient" className={image} src={DoctorWithPatient} />
-        </Col>
-        <Col className="d-flex">
-          <p className={clsx('text-primary', blurb)}>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Autem, aspernatur minima modi
-            quis repellat ullam quaerat nesciunt perspiciatis quos tenetur.
-          </p>
-        </Col>
-      </Row>
-      <Row>
-        <Col className="d-flex">
-          <p className={clsx('text-primary', blurb)}>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Inventore quam quasi quos
-            quibusdam ullam, nam harum voluptatum necessitatibus at deserunt.
-          </p>
-        </Col>
-        <Col className="d-flex">
-          <img alt="Clipboards" className={image} src={Clipboards} />
-        </Col>
-      </Row>
-      <Row>
-        <Col className="d-flex">
-          <img alt="Data Display" className={image} src={DataDisplay} />
-        </Col>
-        <Col className="d-flex">
-          <p className={clsx('text-primary', blurb)}>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Possimus, odit deleniti tenetur
-            in quidem unde voluptas porro ipsam quibusdam perferendis.
-          </p>
-        </Col>
-      </Row>
-    </Container>
-  </Jumbotron>
-);
+const Landing = () => {
+  const [innerWidth, setInnerWidth] = useState(window.innerWidth);
+
+  useEffect(() => {
+    setInnerWidth(window.innerWidth);
+  }, [innerWidth]);
+
+  return (
+    <Jumbotron className={fullHeight}>
+      <Container>
+        <Row>
+          <Col className="d-flex" md="6">
+            <img alt="Doctor with Patient" className={image} src={DoctorWithPatient} />
+          </Col>
+          <Col className="d-flex" md="6">
+            <p className={clsx('text-primary', blurb)}>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Autem, aspernatur minima modi
+              quis repellat ullam quaerat nesciunt perspiciatis quos tenetur.
+            </p>
+          </Col>
+        </Row>
+        {innerWidth < 768 ? (
+          <Row>
+            <Col className="d-flex" md="6">
+              <img alt="Clipboards" className={image} src={Clipboards} />
+            </Col>
+            <Col className="d-flex" md="6">
+              <p className={clsx('text-primary', blurb)}>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Inventore quam quasi quos
+                quibusdam ullam, nam harum voluptatum necessitatibus at deserunt.
+              </p>
+            </Col>
+          </Row>
+        ) : (
+          <Row>
+            <Col className="d-flex" md="6">
+              <p className={clsx('text-primary', blurb)}>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Inventore quam quasi quos
+                quibusdam ullam, nam harum voluptatum necessitatibus at deserunt.
+              </p>
+            </Col>
+            <Col className="d-flex" md="6">
+              <img alt="Clipboards" className={image} src={Clipboards} />
+            </Col>
+          </Row>
+        )}
+        <Row>
+          <Col className="d-flex" md="6">
+            <img alt="Data Display" className={image} src={DataDisplay} />
+          </Col>
+          <Col className="d-flex" md="6">
+            <p className={clsx('text-primary', blurb)}>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Possimus, odit deleniti
+              tenetur in quidem unde voluptas porro ipsam quibusdam perferendis.
+            </p>
+          </Col>
+        </Row>
+      </Container>
+    </Jumbotron>
+  );
+};
 
 export default Landing;
