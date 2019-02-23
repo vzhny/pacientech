7480356d8ff44bcd9589e2f9b7c1fecd9855fa47
Diego Vizhnay <diego@vzhny.com>
2019-02-22 20:53:05 -0500

    Added document title change effect, container to Dashboard page | 02/22

--- src/pages/Dashboard/Dashboard.js
+++ src/pages/Dashboard/Dashboard.js
@@ -1,5 +1,17 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { Container } from 'reactstrap';
+import { dashboard } from './Dashboard.module.scss';
 
-const Dashboard = () => <h1>Dashboard Page</h1>;
+const Dashboard = () => {
+  useEffect(() => {
+    document.title = 'Dashboard | Pacientech';
+  }, []);
+
+  return (
+    <Container className={dashboard}>
+      <h1>Dashboard Page</h1>
+    </Container>
+  );
+};
 
 export default Dashboard;
